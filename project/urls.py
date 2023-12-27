cffc377f9f94e6ee0dc74e21f052380d282e6b3f
DanielScheffel <danioliveira.8292@gmail.com>
2023-12-27 09:16:17 -0300

    Arrumando url

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('cliente.urls')),
+    path('cliente/', include('cliente.urls')),
     path('servicos/', include('servicos.urls')),
     path('funcionarios/', include('equipe.urls')),
     path('ordem_servico/', include('ordemServico.urls')),
